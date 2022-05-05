aa4493b975f9c62953f18418a5b179cdfb26522a
linping <598622595@qq.com>
2022-05-05 16:40:41 +0800

    hystrix接口提交

--- dubbo/src/main/java/com/example/dubbo/controller/HystrixController.java
+++ dubbo/src/main/java/com/example/dubbo/controller/HystrixController.java
@@ -3,7 +3,6 @@ package com.example.dubbo.controller;
 import com.google.common.collect.Lists;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,20 +16,32 @@ public class HystrixController {
     @HystrixCommand(
             fallbackMethod = "command",
             commandProperties = {
-                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "50000"),
+                    //超时时间
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "6000"),
+/*                    //超时时间
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "7000"),*/
+                    //当在配置时间窗口内达到此数量的失败后，进行短路。默认20个
                     @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4"),
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "25"),
+                    //出错百分比阈值，当达到此阈值后，开始短路。默认50%
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    //短路多久以后开始尝试是否恢复，默认5s
                     @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "3000")
             }
     )
     @RequestMapping("/return-test")
-    public String hystrixTest() {
-        List<Integer> list = Lists.newArrayList(2, 5);
-        System.err.println(list.get(2));
+    public String hystrixTest(Integer i) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<Integer> list = Lists.newArrayList(2, 5, 1, 4, 6);
+        System.err.println("i的值: " + i);
+        System.err.println(list.get(i));
         return "正常返回";
     }
 
-    public String command() {
+    public String command(Integer i) {
         return "熔断生效，已隔离！!!";
     }
 }
